1bc31586b9a145dcf422b52ed57a8c041b904248
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-12 22:25:37 -0300

    feat: adding user domain_model

--- lib/hexbird/domain_model/user/users.ex
+++ lib/hexbird/domain_model/user/users.ex
@@ -1,3 +1,10 @@
 defmodule HexbirdWeb.Users do
   use HexbirdWeb, :domain_model
+
+  schema "users" do
+    field :age, :integer
+    field :name, :string
+  end
 end
+
+#TODO - removing ecto from dependencies of domain
